3ba7924b85d5ec100911521ca0fe3d1b140849e6
Doom_hopes <diadia1911@ukr.net>
2020-11-20 13:54:56 +0200

    list view builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,14 @@
 import 'package:flutter/material.dart';
 
-
+void main() =>  runApp(
+    new MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: new Scaffold(
+            body: new ListView.builder(
+              itemBuilder: (context, index) {
+                return new Text('строка $index');
+              },
+            )// ListView.builder
+        )// Scaffold
+    )// MaterialApp
+);
